ca48e175295c101ed9d8101e7d87e220e4e4fc9c
Matthew Gutierrez <matthewgutierrez@Fayleighas-MacBook-Air.local>
2023-10-20 12:16:53 -0500

    reactive stub failure

--- spec/webmock/user_spec.rb
+++ spec/webmock/user_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Metabase::Endpoint::User do
   include_context 'login'
 
+  let(:host) { 'localhost:3030' }
+
   context 'success' do
     it 'Fetches Current User' do
       body = {
@@ -12,26 +14,30 @@ RSpec.describe Metabase::Endpoint::User do
         'last_name'=> 'User'
       }
 
-      stub_request(:get, 'http://localhost:3030/api/user/current')
+      stub_request(:get, "#{host}/api/user/current")
         .to_return(status: 200, body: body.to_json)
 
       begin
         user_json = client.current_user
         user = JSON.parse(user_json)
-  
+
         expect(user['email']).to eq('mb@example.com')
       rescue StandardError
-        expect(WebMock).to have_requested(:put, 'http://localhost:3030/api/user/current')
+        expect(WebMock).to have_requested(:put, "#{host}/api/user/current")
       end
     end
 
+
+    ###################################################################################################################
+    # REACTIVATE USER TEST
+    # #################################################################################################################
     it 'Reactivates User' do
       body = {
         'is_active' => true,
         'id' => 3
       }
 
-      stub_request(:put, 'http://localhost:3030/api/user/3/reactivate')
+      stub_request(:put, "#{host}/api/user/3/reactivate")
         .to_return(status: 200, body: body.to_json)
 
       begin
@@ -40,7 +46,20 @@ RSpec.describe Metabase::Endpoint::User do
 
         expect(user['is_active']).to eq(true)
       rescue StandardError
-        expect(WebMock).to have_requested(:put, 'http://localhost:3030/api/user/3/reactivate')
+        expect(WebMock).to have_requested(:put, "#{host}/api/user/3/reactivate")
+      end
+    end
+
+    context 'user already exists to reactivate' do
+      before do
+        stub_request(:put, "#{host}/api/user/3/reactivate")
+          .to_return(status: 400, body: 'BadRequest')
+      end
+
+      it 'should return error' do
+        message = %r{^PUT http://#{host}/api/user/3/reactivate: 400 - BadRequest}
+        expect { client.reactivate(id: 3) }
+          .to raise_error(Metabase::BadRequest, message)
       end
     end
 
@@ -52,7 +71,7 @@ RSpec.describe Metabase::Endpoint::User do
         'success' => 'true'
       }
 
-      stub_request(:delete, 'http://localhost:3030/api/user/2?id=2')
+      stub_request(:delete, "#{host}/api/user/2?id=2")
         .to_return(status: 200, body: body.to_json)
 
       responce_json = client.delete_user(id: 2)
@@ -67,14 +86,14 @@ RSpec.describe Metabase::Endpoint::User do
         'success' => 'false'
       }
 
-      stub_request(:delete, 'http://localhost:3030/api/user/10?id=10')
+      stub_request(:delete, "#{host}/api/user/10?id=10")
         .to_return(status: 200, body: body.to_json)
 
-      responce_json = client.delete_user(id: 10)
-      responce = JSON.parse(responce_json)
+      response_json = client.delete_user(id: 10)
+      response = JSON.parse(response_json)
 
       # Assert the response and test your code's behavior
-      expect(responce['success']).to eq('false')
+      expect(response['success']).to eq('false')
     end
   end
 end
