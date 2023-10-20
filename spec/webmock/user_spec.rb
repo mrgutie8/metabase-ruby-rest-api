db8f74aadbca5d09cb750080536e712a4ade1f7a
Geena Rustia <geenarustia@yahoo.com>
2023-10-20 09:05:59 -0700

    US52_miniUS#54
    Create new test for GET User

--- spec/webmock/user_spec.rb
+++ spec/webmock/user_spec.rb
@@ -21,5 +21,34 @@ RSpec.describe Metabase::Endpoint::User do
       # Assert the response and test your code's behavior
       expect(user['email']).to eq('mb@example.com')
     end
+
+    ###################################################################################################################
+    # GET USER TEST
+    # #################################################################################################################
+    it 'Fetches a list of Users' do
+      body = [
+        {
+          "email" => "mb@example.com",
+          "first_name"=> "Admin",
+          "id"=> 1,
+          "last_name"=> "User",
+        },
+        {
+          "email" => "mb1@example.com",
+          "first_name"=> "Admin1",
+          "id"=> 2,
+          "last_name"=> "User1",
+        }
+      ]
+
+      stub_request(:get, 'http://localhost:3030/api/user')
+        .to_return(status: 200, body: body.to_json)
+
+      user_json = client.users
+      users = JSON.parse(user_json)
+
+      # Assert the response and test your code's behavior
+      expect(users[0]['email']).to eq('mb@example.com')
+    end
   end
 end
