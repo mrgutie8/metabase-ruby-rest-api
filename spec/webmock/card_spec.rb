3019f81eeded2ec2eeba4c04eaf4cc49d0a941fc
Geena Rustia <geenarustia@yahoo.com>
2023-10-30 18:13:29 -0700

    US-mini task #137
    Create new test for Get card detail

--- spec/webmock/card_spec.rb
+++ spec/webmock/card_spec.rb
@@ -40,6 +40,29 @@ RSpec.describe Metabase::Endpoint::Card do
       end
     end
 
-    
+    it 'Fetches Card detail' do
+      body = {
+        'id' => 1,
+        'name' => 'Card 1',
+        'description' => 'Card 1',
+        'display' => 'Card 1 display',
+        'collection_position' => 1,
+        'collection_id' => 1
+      }
+        
+
+      stub_request(:get, "#{host}/api/card/1")
+      .to_return(status: 200, body: body.to_json)
+      
+
+      begin
+        card_json = client.card(1)
+        card = JSON.parse(card_json)
+
+        expect(card['name']).to eq('Card 1')
+      rescue StandardError
+        expect(WebMock).to have_requested(:put, "#{host}/api/card/1")
+      end
+    end
   end
 end
