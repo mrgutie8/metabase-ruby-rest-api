0365f841e6c9cb2babaa8772356580e5ad14085f
Matthew Gutierrez <matthewgutierrez@Fayleighas-MacBook-Air.local>
2023-10-31 23:57:28 -0500

    public card webmock

---
+++ spec/webmock/pulbic_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Metabase::Endpoint::Public do
+  include_context 'login'
+
+  let(:host) { 'localhost:3030' }
+
+  context 'success' do
+    it 'Logs' do
+
+      stub_request(:get, "#{host}/api/public/card/708a78ec-15c8-405e-b590-de98c6ddfa01")
+        .to_return(status: 200, body: 'OK')
+
+
+      logs_json = client.public_card(card_uuid: '708a78ec-15c8-405e-b590-de98c6ddfa01')
+
+      expect(logs_json).to eq('OK')
+    end
+
+  end
+
+
+end
