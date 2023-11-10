b69110d6e288fd7ae530d306108605786a89c482
Matthew Gutierrez <matthewgutierrez@Fayleighas-MacBook-Air.local>
2023-11-09 21:45:45 -0600

    returns query results of the dataset as specified format

--- spec/webmock/dataset_spec.rb
+++ spec/webmock/dataset_spec.rb
@@ -9,10 +9,23 @@ RSpec.describe Metabase::Endpoint::Dataset do
   describe 'query_dataset' do
     context 'success' do
       it 'should return query results of the dataset' do
-        stub_request(:get, "#{host}/api/dataset/json", headers: { 'Content-Type': 'application/x-www-form-urlencoded' } )
+        stub_request(:post, "#{host}/api/dataset/json")
+          .with(
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          )
           .to_return(status: 200, body: 'OK')
 
-        expect(client.query_dataset(query: query)).to have_requested(:post, "#{host}/api/dataset/json")
+        expect(client.query_dataset(query: query)).to eq('OK')
+      end
+
+      it 'returns query results of the dataset as specified format' do
+        stub_request(:post, "#{host}/api/dataset/csv")
+          .with(
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            )
+          .to_return(status: 200, body: 'OK')
+
+        expect(client.query_dataset(query: query, format: 'csv')).to eq('OK')
       end
     end
   end
