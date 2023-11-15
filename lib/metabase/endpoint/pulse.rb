a7ab728fbb5f5490b5814c4aa37241e3c7ae87b6
apassett <apassett@asu.edu>
2023-11-14 23:50:37 -0500

    US#93_Task#113 Add DELETE subscription method to pulses endpoint

--- lib/metabase/endpoint/pulse.rb
+++ lib/metabase/endpoint/pulse.rb
@@ -11,6 +11,15 @@ module Metabase
       def pulses(**params)
         get('/api/pulse', **params)
       end
+
+      # Delete a subscription.
+      #
+      # @param params [Hash] Query string
+      # @return Parsed response JSON
+      # @see https://github.com/metabase/metabase/blob/master/docs/api-documentation.md#get-apipulse
+      def delete_subscription(**params)
+        get("/api/pulse/#{params[:id]}/subscription", **params)
+      end
     end
   end
 end
