a8bdeb0ad27bc0314dde3e4e1a0535c4f732a5e6
Geena Rustia <geenarustia@yahoo.com>
2023-11-14 22:19:38 -0800

    US#90-mini#107
    Add GET /api/alert/question/:id

--- lib/metabase/endpoint/alert.rb
+++ lib/metabase/endpoint/alert.rb
@@ -11,6 +11,15 @@ module Metabase
       def alerts(**params)
         get('/api/alert', **params)
       end
+
+      # Fetch all questions for the given question(card) id.
+      #
+      # @param params [Hash] Query string
+      # @return [Array<Hash>] Parsed response JSON
+      # @see https://github.com/metabase/metabase/blob/master/docs/api/alert.md#get-apialertquestionid
+      def questions(**params)
+        get("/api/alert/question/#{params[:id]}", **params)
+      end
     end
   end
 end
