6b4139d0378fefc93250d8f76cd6c92749e6796c
Matthew Gutierrez <matthewgutierrez@Fayleighas-MacBook-Air.local>
2023-11-15 23:43:14 -0600

    add post pulse

--- lib/metabase/endpoint/pulse.rb
+++ lib/metabase/endpoint/pulse.rb
@@ -11,6 +11,15 @@ module Metabase
       def pulses(**params)
         get('/api/pulse', **params)
       end
+
+      # Create a new Pulse.
+      #
+      # @param params [Hash] Query string
+      # @return [Array<Hash>] Parsed response JSON
+      # @see https://github.com/metabase/metabase/blob/master/docs/api-documentation.md#get-apipulse
+      def create_pulse(**params)
+        post('/api/pulse', **params)
+      end
     end
   end
 end
