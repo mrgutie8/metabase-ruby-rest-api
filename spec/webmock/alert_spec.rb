f2a3de958ab9f34b1a55912a5fcb5b7f803b7460
Geena Rustia <geenarustia@yahoo.com>
2023-11-17 22:57:13 -0800

    US#92-mini#112
    Add put /api/alert/:id test

--- spec/webmock/alert_spec.rb
+++ spec/webmock/alert_spec.rb
@@ -42,5 +42,39 @@ RSpec.describe Metabase::Endpoint::Alert do
         expect(WebMock).to have_requested(:put, "#{host}/api/alert")
       end
     end
+
+    ###################################################################################################################
+    # PUT ALERT TEST
+    # #################################################################################################################
+    it 'Update a alert with id' do
+      body = {
+        'id' => 1,
+        'alert_condition' => 'goal',
+        'card' => 'id',
+        'alert_first_only' => true,
+        'alert_above_goal' => false,
+        'channels' => [{ 'id' => true }]
+      }
+
+      stub_request(:put, "#{host}/api/alert/1")
+        .to_return(status: 200, body: body.to_json)
+
+      begin
+        response_json = client.update_alert(
+          id: 1,
+          alert_condition: 'goal',
+          card: 'id',
+          alert_first_only: true,
+          alert_above_goal: false,
+          channels: [{ 'id' => true }]
+        )
+        update_alert = JSON.parse(response_json)
+
+        # Assert the response and test your code's behavior
+        expect(update_alert['id']).to eq(1)
+      rescue StandardError
+        expect(WebMock).to have_requested(:put, "#{host}/api/alert")
+      end
+    end
   end
 end
