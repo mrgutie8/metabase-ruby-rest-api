48cd1a01878d601a88b8a271a9dcfeaf8c3c354e
mrgutie8 <mrgutie8@asu.edu>
2024-01-24 17:28:24 -0700

    remove pulse header in test

--- spec/webmock/pulse_spec.rb
+++ spec/webmock/pulse_spec.rb
@@ -98,26 +98,18 @@ RSpec.describe Metabase::Endpoint::Pulse do
         expect(WebMock).to have_requested(:get, 'http://localhost:3030/api/pulse/1')
       end
     end
-  end
 
-  it 'fetches HTML Rendering of a Card with id' do
-    stub_request(:get, "http://localhost:3030/api/pulse/preview_card/1?id=1").
-       with(
-         headers: {
-           'Accept'=>'*/*',
-           'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-           'User-Agent'=>'MetabaseRuby/0.5.0 (ruby3.0.2)',
-           'X-Metabase-Session'=>'bca8d83e-7d37-4670-81b6-560ac452773e'
-         }).
-       to_return(status: 200, body: "", headers: {})
+    it 'fetches HTML Rendering of a Card with id' do
+      stub_request(:get, "http://localhost:3030/api/pulse/preview_card/1?id=1").
+         to_return(status: 200, body: "", headers: {})
 
-    begin
-      pulse_html = client.pulse_preview_card(id: 1)
-      expect(pulse_html).to be_kind_of(String)
-    rescue StandardError
-      expect(WebMock).to have_requested(:get, "#{host}/api/pulse/preview_card/#{id}}")
+      begin
+        pulse_html = client.pulse_preview_card(id: 1)
+        expect(pulse_html).to be_kind_of(String)
+      rescue StandardError
+        expect(WebMock).to have_requested(:get, "#{host}/api/pulse/preview_card/#{id}}")
+      end
     end
-  end
     
     ####################################################################################################################
     # pulse delete_subscription() test
